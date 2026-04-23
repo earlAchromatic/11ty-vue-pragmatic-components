4ee2cc3842e23b6e69e794abf4edefd714144f45
earlAchromatic <68669571+earlAchromatic@users.noreply.github.com>
2026-04-22 22:19:28 -0400

    test header

---
+++ vehicle/common/master/provider/masterlist_provider.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+class MasterlistProvider {
+private:
+  std::vector<std::string> masterlist;
+
+public:
+  MasterlistProvider() {
+    // Initialize with some default items
+    masterlist = {"item1", "item2", "item3"};
+  }
+
+  void addItem(const std::string& item) {
+    masterlist.push_back(item);
+  }
+
+  std::vector<std::string> getMasterlist() const {
+    return masterlist;
+  }
+
+  void printMasterlist() const {
+    for (const auto& item : masterlist) {
+      std::cout << item << std::endl;
+    }
+  }
+};
