ba77c40c5c1a82c40d0cb6666490aa5c7132a82e
earlAchromatic <68669571+earlAchromatic@users.noreply.github.com>
2026-04-22 22:43:38 -0400

    Adjust getMasterlist comment wording

--- vehicle/common/master/provider/masterlist_provider.cpp
+++ vehicle/common/master/provider/masterlist_provider.cpp
@@ -18,7 +18,7 @@ public:
   }
 
   std::vector<std::string> getMasterlist() const {
-    // Return a copy of the provider's current in-memory masterlist.
+    // Return a copy of the provider's current in-memory item list.
     return masterlist;
   }
 
